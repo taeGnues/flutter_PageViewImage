8cc64db61e5aea76df8f79d22f7f52eef49d597e
정태승 <hi563@g.skku.edu>
2023-02-16 17:45:58 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:image_carousel/screen/home_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    ),
-  );
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
 }
